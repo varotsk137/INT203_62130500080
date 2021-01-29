7c79e64549a3a46ff5c2dfef634602681709f1cc
Varot Tirakhuntorn <varot.137@mail.kmutt.ac.th>
2021-01-29 20:59:00 +0700

    [Update] Add background images with change script. Change some class attribute.

--- main.js
+++ main.js
@@ -7,3 +7,16 @@ const app = {
 
 }
 Vue.createApp(app).mount('#app')
+
+let body = document.getElementById("body");
+let i = Math.floor(Math.random() * 8 + 1);
+
+function changeImage() {
+    i++;
+    if(i==9) i=1;
+    let attr = "background-image: url(images/bg_imgs/" + i + ".jpg);";
+    body.setAttribute("style", attr);
+    setTimeout(changeImage, 7000);
+}
+
+changeImage();
